fe7b2465375fcd354ec3073f96ec74825f5ca8b1
kalibuas <Saltedasin123!@#>
2023-03-24 23:28:27 +0800

    clean up

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,18 +1,13 @@
 import '../../globals.css'
 import type { AppProps } from 'next/app'
-
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query'
-
-const queryClient = new QueryClient()
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-      <QueryClientProvider client={queryClient}>
-        <Component {...pageProps} />
-      </QueryClientProvider>
+    <>
+    <Component {...pageProps} />
+    <Analytics />
+    </>
+    )
 
-  )
 }
--- store/index.ts
+++
@@ -1,8 +0,0 @@
-import { create } from 'zustand'
-
-export const useChatStore = create((set) => ({
-    messages: [],
-    // @ts-ignore
-    addMessage: (message: string) => set((state) => ({ messages: [...state.messages, message] })),
-    clearMessages: () => set(() => ({ messages: [] })),
-}))
