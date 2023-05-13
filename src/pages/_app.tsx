c00b28f9140545c00a32561779d733d89a9b79ee
Karl Kenneth Alibuas <kalibuas@gmail.com>
2023-05-13 23:02:42 +0800

    added script

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import '../../globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-    <Component {...pageProps} />
-    <Analytics />
+      <Component {...pageProps} />
+      <Script src="https://missional-analytics.vercel.app/script.js" data-website-id="cd44b376-bdc0-4ff5-a310-a6d5599feb8b" />
+      <Analytics />
     </>
-    )
+  )
 
 }
