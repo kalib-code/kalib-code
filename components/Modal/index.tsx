4662e05feeefb192b02877a5e3f449f0d80044d3
Karl Kenneth Alibuas <kalibuas@gmail.com>
2023-09-09 02:42:48 +0000

    added resume

--- components/Modal/index.tsx
+++ components/Modal/index.tsx
@@ -2,8 +2,7 @@ import { BsFillSendFill } from "react-icons/bs";
 
 import { TypeAnimation } from 'react-type-animation';
 import Link from "next/link";
-// import {BsGithub, BsLinkedin, BsMedium} from "react-icons/bs";
-// import {FaNpm} from "react-icons/fa";
+
 
 interface Props {
     message: any
@@ -23,36 +22,6 @@ export const Modal = (props: Props) => {
     const onChangeToggle = (e: any) => {
         onClickModal()
     }
-
-
-
-    // get all the link in a string and put in a array
-    // const getLink = (str: string) => {
-    //     const regex = /https?:\/\/[^\s]+/g;
-    //     return str?.match(regex);
-    // }
-
-    // const showLink = (str: string) => {
-    //     switch (str) {
-    //         case 'https://www.linkedin.com/in/kalibuas/':
-    //             return <Link className={"badge badge-lg mr-4"} href={str}><BsLinkedin size={30}/></Link>
-    //         case 'https://medium.com/@kalib-stacks':
-    //             return <Link className={"badge badge-lg mr-4"} href={str}><BsMedium size={30}/></Link>
-    //         case 'https://www.npmjs.com/~marculosus':
-    //             return<Link className={"badge badge-lg mr-4"} href={str}> <FaNpm size={30}/></Link>
-    //         case 'https://github.com/kalib-code':
-    //             return<Link className={"badge badge-lg mr-4"} href={str}> <BsGithub size={30}/></Link>
-    //
-    //     }
-    // }
-    // // @ts-ignore
-    // const linksRender=  getLink(chat as string)?.map((link: string, index) => {
-    //     return (
-    //         <div key={index}>
-    //             {showLink(link)}
-    //         </div>
-    //     )
-    // })
 
     return (
         <>
@@ -90,16 +59,8 @@ export const Modal = (props: Props) => {
                             />
                         </article> : 'Loading...'}
                     </div>
-                    {/*<div className="w-full justify-center content-center">*/}
-                    {/*    /!* @ts-ignore center  *!/*/}
-                    {/*    {getLink(chat as string) ?<div className="flex flex-row">*/}
-                    {/*        {linksRender}*/}
-                    {/*    </div>: null*/}
-                    {/*    }*/}
-
-                    {/*</div>*/}
                     <p className="text-center"><Link
-                        href="https://drive.google.com/file/d/1nwKypNkc8mRadrACedHsC_JqLJiyRa4T/view?usp=sharing"
+                        href="https://drive.google.com/file/d/1bYfVy7tUvBn1pwmRTWMCY1LhcvfYMETi/view?usp=sharing"
                         data-umami-event="Download Resume"
                         className="link">Download Resume</Link></p>
                 </label>
